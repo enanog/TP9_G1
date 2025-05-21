63bec657e53affefd76c52069449f5b2e5922d79
Dylan <dyfrigerio@AWSNV-L-VDA06.itbadc.itba.edu.ar>
2025-05-21 10:54:40 -0300

    General main (not finish )

---
+++ main.c
@@ -0,0 +1,65 @@
+/* ---------------------------------------------------
+ * TP9:Tipos de datos avanzados y Herramientas de
+ * desarrollo.
+ * ---------------------------------------------------
+ * GRUPO 1:
+ * 	CASTRO, Tomás
+ * 	FRIGERIO, Dylan
+ * 	VALENZUELA, Agustín
+ * 	YAGGI, Lucca
+ *
+ * Profesores:
+ * 	MAGLIIOLA, Nicolas
+ * 	JACOBY, Daniel
+ * 	VACATELLO, Pablo
+ *
+ * fecha: 26/05/2025
+ * ---------------------------------------------------*/
+
+#include <stdio.h>
+#include <stdint.h>
+
+enum{
+	LED_SELECTOR = 0,
+	NOT_LED,
+	OFF_ALL,
+	ON_ALL,
+	FINISH
+};
+
+enum{
+	STOP = 0,
+	RUNNING,
+};
+
+int main(void)
+{
+	uint8_t prog = RUNNING;
+
+	while(prog)
+	{
+		switch(prog)
+		{
+			case LED_SELECTOR:
+				break;
+
+			case NOT_LED:
+				break;
+
+			case OFF_ALL:
+				break;
+
+			case ON_ALL:
+				break;
+
+			case FINISH:
+				prog = STOP;
+				break;
+
+			default:
+				printf("Se ingreso un comando erroneo.\n");
+				break;
+		}
+	}
+	return 0;
+}
