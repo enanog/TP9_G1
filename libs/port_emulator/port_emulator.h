a84795627778ac7c5694ab5a0095df26a5bdf432
Agustin Valenzuela <valenzuela.agustin.tomas@gmail.com>
2025-05-21 10:29:31 -0300

    port_emulator template (not finished)

---
+++ libs/port_emulator/port_emulator.h
@@ -0,0 +1,18 @@
+#ifndef PORT_EMULATOR_H
+#define PORT_EMULATOR_H
+
+typedef struct
+{
+	
+}port_t;
+
+
+#define bitSet
+#define bitClr
+#define bitGet
+#define bitToggle
+#define maskOn
+#define maskOff
+#define maskToggle
+
+#endif
