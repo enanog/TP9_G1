c2610ea018508cb216674a1bf6e08ff63a1b191d
Dylan Ezequiel Frigerio <dyfrigerio@AWSNV-L-VDA05.itbadc.itba.edu.ar>
2025-05-23 09:41:00 -0300

    Agrego nstdio

---
+++ libs/nstdio/nstdio.c
@@ -0,0 +1,16 @@
+/* ---------------------------------------------------
+ * nstdio.c
+ * ---------------------------------------------------
+ * GRUPO 1:
+ * 	CASTRO, Tomás
+ *	FRIGERIO, Dylan
+ * 	VALENZUELA, Agustín
+ * 	YAGGI, Lucca
+ *
+ * Profesores:
+ * 	MAGLIIOLA, Nicolas
+ * 	JACOBY, Daniel
+ * 	VACATELLO, Pablo
+ *
+ * fecha: 21/05/2025
+ * ---------------------------------------------------*/
---
+++ libs/nstdio/nstdio.h
@@ -0,0 +1,25 @@
+/* ---------------------------------------------------
+ * nstdio.h
+ * ---------------------------------------------------
+ * GRUPO 1:
+ * 	CASTRO, Tomás
+ *	FRIGERIO, Dylan
+ * 	VALENZUELA, Agustín
+ * 	YAGGI, Lucca
+ *
+ * Profesores:
+ * 	MAGLIIOLA, Nicolas
+ * 	JACOBY, Daniel
+ * 	VACATELLO, Pablo
+ *
+ * fecha: 21/05/2025
+ * ---------------------------------------------------*/
+
+#ifndef _NSTDIO_H_
+#define _NSTDIO_H_
+
+
+
+
+
+#endif /* LIBS_NSTDIO_NSTDIO_H_ */
